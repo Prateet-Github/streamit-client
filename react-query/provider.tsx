3f09e63f60e310cedcdd17aebc7aaf33f6cdc5d3
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-07 00:51:15 +0530

    feat: add React Query Devtools for enhanced debugging and update dependencies

--- react-query/provider.tsx
+++ react-query/provider.tsx
@@ -3,11 +3,17 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 import { queryClient } from "./queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function ReactQueryProvider({ children }: { children: ReactNode }) {
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
