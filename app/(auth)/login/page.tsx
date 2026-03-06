a625ebda48c83f49aded224aa14782fb27b28639
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-06 16:57:12 +0530

    feat: add authentication layout and pages for login and registration

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { Lock, Mail } from "lucide-react";
+
+const Login = () => {
+  return (
+    <section className="text-center flex flex-col gap-4 border border-white/40  p-8 rounded-lg shadow-md w-full max-w-md mx-4">
+      <h1 className="text-2xl font-extrabold">Welcome back</h1>
+      <form className="flex flex-col gap-4 items-center">
+        <div className="relative w-full">
+          <Mail className="absolute left-4 top-1/2 -translate-y-1/2 text-white/60 w-5 h-5" />
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="border border-white/40 p-4 pl-12 rounded-full w-full focus:outline-none"
+          />
+        </div>
+        <div className="relative w-full">
+          <Lock className="absolute left-4 top-1/2 -translate-y-1/2 text-white/60 w-5 h-5" />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="border border-white/40 p-4 pl-12 rounded-full w-full focus:outline-none"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-purple-700 p-4 rounded-full w-full font-bold text-white cursor-pointer hover:bg-purple-600 transition"
+        >
+          Sign In
+        </button>
+      </form>
+      <p className="text-xml text-gray-500">
+        Don't have an account?{" "}
+        <span className="underline cursor-pointer font-semibold text-purple-500 hover:text-purple-700 transition">
+          <Link href="/register">Sign Up</Link>
+        </span>
+      </p>
+    </section>
+  );
+};
+
+export default Login;
