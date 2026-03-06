a9f644bc9e55a64b5ca4c8ee7efdaa63b6de88f2
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-06 19:25:17 +0530

    feat: integrate React Query for data fetching and add provider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import ReactQueryProvider from "@/react-query/provider";
 
 export const metadata: Metadata = {
   title: "StreamIt",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
