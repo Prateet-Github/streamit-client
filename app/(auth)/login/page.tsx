a677b2ed0d361b11b8a99133e1734863513ff664
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-13 01:59:58 +0530

    feat: refactor AuthLayout and update styling for Login and Register pages for improved layout and user experience

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -43,51 +43,59 @@ const Login = () => {
   };
 
   return (
-    <section className="text-center flex flex-col gap-4 border border-green-500  p-8 rounded-lg shadow-md w-full max-w-md mx-4">
-      <h1 className="text-3xl font-extrabold text-green-500">Welcome back</h1>
-      <p className="text-gray-400 text-sm">
-        Sign in to your account to continue enjoying StreamIt
-      </p>
-      <form
-        className="flex flex-col gap-5 items-center"
-        onSubmit={handleSubmit}
-      >
-        <div className="relative w-full">
-          <Mail className="absolute left-4 top-1/2 -translate-y-1/2 text-white/60 w-5 h-5" />
+    <main className="flex min-h-screen justify-center">
+      <div className="hidden md:flex items-center justify-center w-1/2 bg-green-500" />
 
-          <input
-            type="email"
-            placeholder="Email"
-            className="border border-green-500 p-4 pl-12 rounded-full w-full focus:outline-none focus:ring-2 focus:ring-green-500"
-            name="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="relative w-full">
-          <Lock className="absolute left-4 top-1/2 -translate-y-1/2 text-white/60 w-5 h-5" />
+      <div className="flex items-center justify-center md:w-1/2 px-6">
+        <section className="text-center flex flex-col gap-4 border border-green-500  p-8 rounded-lg shadow-md w-full max-w-md">
+          <h1 className="text-3xl font-extrabold text-green-500">
+            Welcome back
+          </h1>
+          <p className="text-gray-400 text-sm">
+            Sign in to your account to continue enjoying StreamIt
+          </p>
+          <form
+            className="flex flex-col gap-5 items-center"
+            onSubmit={handleSubmit}
+          >
+            <div className="relative w-full">
+              <Mail className="absolute left-4 top-1/2 -translate-y-1/2 text-white/60 w-5 h-5" />
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="border border-green-500 p-4 pl-12 rounded-full w-full focus:outline-none focus:ring-2 focus:ring-green-500"
-            name="password"
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          disabled={loginMutation.isPending}
-          className="bg-green-600 p-4 rounded-full w-full font-bold text-white cursor-pointer hover:bg-green-700 transition disabled:opacity-60 disabled:cursor-not-allowed"
-        >
-          {loginMutation.isPending ? "Signing In..." : "Sign In"}
-        </button>
-      </form>
-      <p className="text-xml text-gray-500">
-        Don't have an account?{" "}
-        <span className="underline cursor-pointer font-semibold text-green-500 hover:text-green-600 transition">
-          <Link href="/register">Sign Up</Link>
-        </span>
-      </p>
-    </section>
+              <input
+                type="email"
+                placeholder="Email"
+                className="border border-green-500 p-4 pl-12 rounded-full w-full focus:outline-none focus:ring-2 focus:ring-green-500"
+                name="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="relative w-full">
+              <Lock className="absolute left-4 top-1/2 -translate-y-1/2 text-white/60 w-5 h-5" />
+
+              <input
+                type="password"
+                placeholder="Password"
+                className="border border-green-500 p-4 pl-12 rounded-full w-full focus:outline-none focus:ring-2 focus:ring-green-500"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            <button
+              disabled={loginMutation.isPending}
+              className="bg-green-600 p-4 rounded-full w-full font-bold text-white cursor-pointer hover:bg-green-700 transition disabled:opacity-60 disabled:cursor-not-allowed"
+            >
+              {loginMutation.isPending ? "Signing In..." : "Sign In"}
+            </button>
+          </form>
+          <p className="text-sm text-gray-500">
+            Don't have an account?{" "}
+            <span className="underline cursor-pointer font-semibold text-green-500 hover:text-green-600 transition">
+              <Link href="/register">Sign Up</Link>
+            </span>
+          </p>
+        </section>
+      </div>
+    </main>
   );
 };
 
